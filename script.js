0e6001562b6b5e125859feed23be7db107421d49
Lunehh <lawasuflas@gmail.com>
2025-01-01 13:51:36 -0300

    aa

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ window.addEventListener("load", function() {
     bgm.play().catch(error => {
         console.log("Music failed to autoplay:", error);
     });
-});
-
-// Toggle pop-up on paw click
-document.getElementById('easter-egg').addEventListener('click', () => {
+ });
+ 
+ // Toggle pop-up on paw click
+ document.getElementById('easter-egg').addEventListener('click', () => {
     const popup = document.getElementById('popup');
     popup.classList.toggle('hidden');
-});
-
-// Make the ASCII art center on page load
-window.onload = function() {
+ });
+ 
+ // Make the ASCII art center on page load
+ window.onload = function() {
     const asciiArt = document.getElementById('ascii-art');
     asciiArt.style.whiteSpace = 'pre-wrap'; // Ensures it formats correctly
-};
+ };
+ 

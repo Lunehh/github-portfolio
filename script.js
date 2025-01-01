eda51df033f9b29c92a366b092454dbc4c3fec42
Lunehh <lawasuflas@gmail.com>
2025-01-01 13:33:35 -0300

    idk manm

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Play music automatically when the page loads
-const music = document.getElementById('background-music');
-
-// Ensure music plays if the browser blocks autoplay
-music.play().catch((error) => {
-    console.log("Autoplay was blocked, playing on user interaction.");
-});
-
-// Toggle the popup when the paw icon is clicked
 document.getElementById('easter-egg').addEventListener('click', () => {
     const popup = document.getElementById('popup');
     popup.classList.toggle('hidden');
